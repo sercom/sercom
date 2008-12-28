fd3220660d04886a1f3caf59c01f1055ff1bc973
Pablo Daniel Roca <pablo@7542.fi.uba.ar>
2008-12-28 15:56:29 -0200

    Correccion de elementos a mostrar en tinyMCE, agregado de tablas y cut and paste.

--- sercom/static/js/init_tiny_mce.js
+++ sercom/static/js/init_tiny_mce.js
@@ -1,7 +1,13 @@
 tinyMCE.init(
 	{
 		mode:"textareas",
-		theme:"simple"
+		theme:"advanced",
+		plugins:"table,paste",
+		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|formatselect,fontselect,fontsizeselect,|,charmap,|,removeformat",
+		theme_advanced_buttons2 : "cut,copy,paste,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,image,code,|,forecolor,backcolor,|,table",
+		theme_advanced_buttons3 : "",
+		invalid_elements:"hr,removeformat,visualaid,sub,sup"
+
 	}
 	);
 
