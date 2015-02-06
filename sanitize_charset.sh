1c063342c23cc1bf448959009fb959d93501b8aa
root <root@ubuntu.(none)>
2015-02-05 22:25:22 -0300

    Fixed encoding errors in sanitize script for sourcecode PDF

--- sanitize_charset.sh
+++ sanitize_charset.sh
@@ -9,18 +9,16 @@ then
   exit 1
 fi
 
-if [ "convert" != "$1" ]; then
-  echo "ATENCIÓN: Este script cambiará la codificación de todos los archivos del directorio actual a ISO-8859-1."
-  echo "Para que la conversión tenga lugar debe pasar 'convert' como primer argumento."
-  exit 1
+file=$1
+
+iconv -futf8 -tlatin1 "$file" > "$file.conv"
+if [ $? == 0 ]; then
+    echo "Converting File: $file. SUCCESS"
+    mv "$file.conv" "$file"
+    exit 0
+else
+    echo "Converting File: $file. FAIL"
+    rm "$file.conv"
+    exit 1
 fi
 
-for F in `ls`;
-do
-    iconv -futf8 -tlatin1 $F > $F'__utf8'
-    if [ $? == 0 ]; then
-        mv $F'__utf8' $F
-    else
-        rm $F'__utf8'
-    fi
-done
